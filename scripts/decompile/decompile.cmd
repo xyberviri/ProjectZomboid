b389e7260ad2ed5e41672d9d9434d03243780c63
James Velasquez <xyberviri@gmail.com>
2024-02-04 15:56:11 -0500

    Add files via upload

---
+++ scripts/decompile/decompile.cmd
@@ -0,0 +1,1 @@
+E:\ProjectZomboid\jdk-18\bin\java.exe -cp "C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2020.3\plugins\java-decompiler\lib\java-decompiler.jar" org.jetbrains.java.decompiler.main.decompiler.ConsoleDecompiler -hdc=0 -dgs=1 -rsy=1 -rbr=1 -lit=1 -nls=1 -mpm=60 %*
---
+++ scripts/decompile/decompile_PZ.bat
@@ -0,0 +1,3 @@
+decompile.cmd "C:\Program Files (x86)\Steam\steamapps\common\ProjectZomboid\zombie" E:\ProjectZomboid\src\zombie
+
+pause
